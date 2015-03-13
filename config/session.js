9195566230848de10f9a38b227918e1b2707b07a
松村 真咲 <mm.schwarz@gmail.com>
2015-03-13 21:12:51 +0900

    session だけ redis に戻す

--- config/session.js
+++ config/session.js
@@ -41,7 +41,7 @@ module.exports.session = {
   * session store that can be shared across multiple Sails.js servers        *
   ***************************************************************************/
 
-  adapter: 'socket.io-redis',
+  adapter: 'redis',
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   db: process.env.REDIS_DB,
