004375a0283073571bd00099db96d05a17d53f2a
松村 真咲 <mm.schwarz@gmail.com>
2015-03-13 20:52:51 +0900

    RangeError: Maximum call stack size exceeded は heroku で socket.io-redis 使った時の問題っぽい？

--- config/sockets.js
+++ config/sockets.js
@@ -44,7 +44,7 @@ module.exports.sockets = {
   // -OR-
   //
 
-  adapter: 'socket.io-redis',
+  adapter: 'redis',
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   db: process.env.REDIS_DB,
